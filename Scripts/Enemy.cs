6b88b3a2ca9043f7128860a20f5e6a9e7d98e6f7
Mason Holland <MasonTHolland@outlook.com>
2019-10-17 16:42:49 -0600

    adds scripts from lessons thus far

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        // if bottom of screen
+        // respawn at top with a new random x position
+        if (transform.position.y <= -5.3)
+        {
+            float randX = Random.Range(-8.5f, 8.5f);
+            transform.position = new Vector3(randX, 6.95f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
+            Destroy(this.gameObject);
+        }
+        else if (other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+  
+    }
+}
