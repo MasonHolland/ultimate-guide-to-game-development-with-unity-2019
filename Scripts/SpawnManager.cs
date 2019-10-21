ccd2ef8240a23739852c78ce2142bbba277d4366
Mason Holland <MasonTHolland@outlook.com>
2019-10-21 15:49:42 -0600

    adds spawn manager logic

--- Scripts/SpawnManager.cs
+++ Scripts/SpawnManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f. 8f), 7, 0);
+            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
     }
 }
